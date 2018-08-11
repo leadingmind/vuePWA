8dfc629a5dc733255027c395b9fa60fb3116767a
rage <rage.manish@gmail.com>
2018-08-11 21:00:14 +0530

    Updates

---
+++ precache-manifest.1131fcdfb19d40681c2efde6a914fd05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "041a2d231bae72ebdc66",
+    "url": "/js/chunk-vendors.041a2d23.js"
+  },
+  {
+    "revision": "34dcf03e382d734aa38d",
+    "url": "/js/app.34dcf03e.js"
+  },
+  {
+    "revision": "13e1ca4f77def6d16dd9a0079080e276",
+    "url": "/index.html"
+  },
+  {
+    "revision": "536ddcbcb217805c83a93c45ea8b8e88",
+    "url": "/img/default.536ddcbc.jpg"
+  },
+  {
+    "revision": "34dcf03e382d734aa38d",
+    "url": "/css/app.4ab803f1.css"
+  }
+];
