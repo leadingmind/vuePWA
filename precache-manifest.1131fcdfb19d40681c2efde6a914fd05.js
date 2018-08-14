77b74441a47e9185eefa4ebd46ba44e1dc5ff9d6
rage <rage.manish@gmail.com>
2018-08-14 23:07:51 +0530

    Updates

--- precache-manifest.1131fcdfb19d40681c2efde6a914fd05.js
+++ precache-manifest.1131fcdfb19d40681c2efde6a914fd05.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/vuePWA/robots.txt"
+    "url": "/robots.txt"
   },
   {
     "revision": "041a2d231bae72ebdc66",
-    "url": "/vuePWA/js/chunk-vendors.041a2d23.js"
+    "url": "/js/chunk-vendors.041a2d23.js"
   },
   {
     "revision": "34dcf03e382d734aa38d",
-    "url": "/vuePWA/js/app.34dcf03e.js"
+    "url": "/js/app.34dcf03e.js"
   },
   {
     "revision": "13e1ca4f77def6d16dd9a0079080e276",
-    "url": "/vuePWA/index.html"
+    "url": "/index.html"
   },
   {
     "revision": "536ddcbcb217805c83a93c45ea8b8e88",
-    "url": "/vuePWA/img/default.536ddcbc.jpg"
+    "url": "/img/default.536ddcbc.jpg"
   },
   {
     "revision": "34dcf03e382d734aa38d",
-    "url": "/vuePWA/css/app.4ab803f1.css"
+    "url": "/css/app.4ab803f1.css"
   }
 ];
